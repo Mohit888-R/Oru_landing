20abd0afa9d43df890d2bf06162f71bba139a7d7
mohit singh <naruka.mohit88@gmail.com>
2023-11-24 16:40:21 +0530

    schedule time in timer clock

--- src/components/TimeStamp.tsx
+++ src/components/TimeStamp.tsx
@@ -1,6 +1,6 @@
 import Countdown from "react-countdown";
 
-const targetDate = new Date("2023-11-10T00:00:00Z");
+const targetDate = new Date("2023-12-01T00:00:00Z");
 
 const formatNumber = (number?: number) =>
   number !== undefined && number < 10 ? `0${number}` : number;
@@ -24,10 +24,15 @@ export const renderer = ({
   completed?: boolean;
 }) => {
   if (completed) {
-    return <span>Countdown expired!</span>;
+    return  <div className="text-[24px] lg:text-[42px]">
+ORUphones is there!!!
+  </div>;
   } else {
     return (
       <div>
+          <div className="text-[24px] lg:text-[42px]">
+          Website will be live in
+        </div>
         {formatNumber(days)}
         <TextChar char="d" />:{formatNumber(hours)}
         <TextChar char="h" />:{formatNumber(minutes)}
@@ -41,9 +46,6 @@ export const TimeStamp = () => {
   return (
     <div className=" bg-[#F6C018]">
       <div className="flex space-x-4  py-16  flex-col lg:flex-row justify-center items-center text-center container px-16">
-        <div className="text-[24px] lg:text-[42px]">
-          Website will be live in
-        </div>
         <div className="font-bold text-[32px] lg:text-[64px]">
           {" "}
           <Countdown date={targetDate} renderer={renderer} />
